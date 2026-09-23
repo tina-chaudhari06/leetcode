f7b779386f3de292c953b43dd441da8c7b06604b
tina-chaudhari06 <tina.rc2006@gmail.com>
2026-09-23 20:15:22 +0530

    Time: 673 ms (68.08%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT ROUND(COUNT(CASE WHEN order_date = customer_pref_delivery_date THEN 1 END)*100 / COUNT(*), 2) immediate_percentage  
+SELECT ROUND(SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END)*100 / COUNT(*), 2) immediate_percentage  
 FROM Delivery
 WHERE(customer_id, order_date) IN (
     SELECT customer_id, MIN(order_date)
