c01abb51e4a7fd7d4700f44fc088a236de6ca377
tina-chaudhari06 <tina.rc2006@gmail.com>
2026-09-26 00:02:02 +0530

    Time: 291 ms (85.11%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-SELECT x, y, z, (CASE WHEN x+y>z AND x+z>y AND y+z>x THEN 'Yes' ELSE 'No' END) triangle 
+SELECT *, IF(x+y>z AND x+z>y AND y+z>x, 'Yes', 'No') triangle 
 FROM Triangle;
