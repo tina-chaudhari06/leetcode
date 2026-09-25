ed9ab722b8949cdbebdd756c9d0311747df727d2
tina-chaudhari06 <tina.rc2006@gmail.com>
2026-09-26 02:14:53 +0530

    Time: 583 ms (96.63%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,10 +1,9 @@
 DELETE FROM Person
-WHERE id IN(
+WHERE id NOT IN(
 SELECT id
 FROM (
-    SELECT p2.id
-    FROM Person p1, Person p2
-    WHERE p1.email = p2.email
-    AND p1.id < p2.id
+    SELECT MIN(id) id
+    FROM Person 
+    GROUP BY email
 ) d
 );
