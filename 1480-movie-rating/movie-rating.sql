72639f0a6eddbf3fcd00c204cc48aca8c2b818a8
tina-chaudhari06 <tina.rc2006@gmail.com>
2026-09-27 01:16:56 +0530

    Time: 1148 ms (99.29%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,26 +1,18 @@
-# Write your MySQL query statement below
-(SELECT name results
-FROM (
-    SELECT name, count(*) c
-    FROM Users u
-    INNER JOIN MovieRating m
-    ON u.user_id = m.user_id
-    GROUP BY u.user_id
-    
-) a
-ORDER BY c DESC, name
-LIMIT 1)
+(SELECT name AS results
+ FROM Users u
+ INNER JOIN MovieRating m
+ ON u.user_id = m.user_id
+ GROUP BY u.user_id, u.name
+ ORDER BY COUNT(*) DESC, name ASC
+ LIMIT 1)
 
-UNION ALL
+UNION ALL 
 
-(SELECT title results
-FROM(
-    SELECT m.movie_id, title, AVG(rating) a
-    FROM Movies m 
-    INNER JOIN MovieRating r
-    ON m.movie_id = r.movie_id
-    WHERE created_at LIKE '2020-02%'
-    GROUP BY m.movie_id
-) q
-ORDER BY a DESC, title
-LIMIT 1)
+(SELECT title AS results
+ FROM Movies m
+ INNER JOIN MovieRating r
+ ON m.movie_id = r.movie_id
+ WHERE created_at LIKE '2020-02%'
+ GROUP BY m.movie_id, title
+ ORDER BY AVG(rating) DESC, title ASC
+ LIMIT 1);
